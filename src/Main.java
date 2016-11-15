06aa261b56f0c8184a1e064b80ff445d0fc3926a
Kyle Rudy <kdrudy@kyles-mbp.apollosrc.root>
2016-11-15 11:03:15 -0600

    Second day

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        Person.isValidName("Bob Johnson");
 
-        boolean isKyle = true;
-        int score = 900;
+        System.out.println(Person.hair);
 
-        String name = "Kyle";
-
-
-        /*
-
-        switch(name) {
-            case "Kyle":
-            case "Bob":
-                System.out.println("Less than or equal to one");
-                break;
-            case "Gerald":
-                System.out.println("Greater than 1");
-                break;
-            default:
-                System.out.println("ANother number");
-        }
-*/
-
-        try {
-            mightThrowException(false);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-        System.out.println("The end");
-
-    }
-
-    public static void mightThrowException(boolean shouldThrow) throws Exception {
-        if (shouldThrow) {
-             throw new Exception("exception!");
-        }
-        System.out.println("No Exception");
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,52 @@
  * Created by kdrudy on 8/29/16.
  */
 public class Person {
-    
+
     private String name;
     private int age;
     private boolean isAlive;
 
+    public static String hair = "Brown";
+
+    public Person() {
+        name = "Kyle";
+    }
+
+    public Person(String n) {
+        if(isValidName(n)) {
+            name = n;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        if(isValidName(n)) {
+            name = n;
+        }
+    }
+
+    public static boolean isValidName(String name) {
+        return name.contains(" ") && !name.isEmpty() && name.length() <= 20;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        if(a > 0 && a < 150) {
+            age = a;
+        }
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
