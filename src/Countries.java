4f9d9cf7063d71d3240d8189558b961b5d0a42d2
Kyle Rudy <kdrudy@kyles-mbp.apollosrc.root>
2016-11-29 09:08:08 -0600

    Adding additional code

---
+++ src/Countries.java
@@ -0,0 +1,33 @@
+/**
+ * Created by kdrudy on 11/28/16.
+ */
+public class Countries {
+
+    //Hashmap<String, ArrayList<Country>
+
+    //Create file object and scanner
+
+    //while the file has more stuff
+        //read each line
+        //split each line into an array
+        //use the array to create a new Country
+        //figure out first letter of the country name
+        //if hashmap contains the first letter as a key
+            //Arraylist<Country> temp = get the arraylist out of the hashmap using the first letter as a key
+            //add our new Country to that arraylist
+        //else hashmaps does not contain the first letter as a key
+            //create a new arraylist
+            //add our new Country to that arraylist
+            //put the arraylist into our hashmap using the first letter as a key
+
+    //ask the user for a letter they want the countries
+    //if they haven't entered a single letter
+        //throw an exception
+    //else
+        //get the arraylist out of the hashmap using the entered letter as the key
+        //create file object with correct name
+        //iterate through arraylist
+            //write out each country to a new line in our file
+        //close the file
+
+}
